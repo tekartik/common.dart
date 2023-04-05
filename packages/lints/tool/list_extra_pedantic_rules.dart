69ba1047ffa64aa4b9a24a81a7f1bf167b2c1b27
Alexandre Roux <alex@tekartik.com>
2023-04-05 10:18:49 +0200

    feat: swith to dart flutter team lints

--- packages/lints/tool/list_extra_pedantic_rules.dart
+++ packages/lints/tool/list_extra_pedantic_rules.dart
@@ -1,27 +1,8 @@
-import 'dart:convert';
 import 'dart:io';
 
+import 'package:dev_test/build_support.dart';
 import 'package:path/path.dart';
 import 'package:yaml/yaml.dart';
-
-/// Read info from `.packages` file, key being the package, value being the path
-Future<Map<String, String>> getDotPackagesMap(String packageRoot) async {
-  final yamlPath = join(packageRoot, '.packages');
-  final content = await File(yamlPath).readAsString();
-
-  final map = <String, String>{};
-  final lines = LineSplitter.split(content);
-  for (var line in lines) {
-    line = line.trim();
-    if (!line.startsWith('#')) {
-      final separator = line.indexOf(':');
-      if (separator != -1) {
-        map[line.substring(0, separator)] = line.substring(separator + 1);
-      }
-    }
-  }
-  return map;
-}
 
 Future<List<String>> getRules(String path) async {
   var yaml = loadYaml(await File(path).readAsString()) as Map;
@@ -41,9 +22,9 @@ Future<List<String>> getRules(String path) async {
 
 Future<void> _writeRules(String name, List<String> rules) async {
   var sb = StringBuffer();
-  for (var element in rules) {
-    sb.writeln('  - $element');
-  }
+  //for (var element in rules) {
+  //  sb.writeln('  - $element');
+  //}
   for (var element in rules) {
     sb.writeln('    $element: true');
   }
@@ -55,35 +36,44 @@ Future<void> main() async {
   var rules = await getRules(join('lib', 'recommended.yaml'));
   await _writeRules('tekartik', rules);
 
-  var dotPackagesMap = await getDotPackagesMap('.');
+  var packageConfigMap = await pathGetPackageConfigMap('.');
+  var path = '.';
 
-  var pedanticLibPath = Uri.parse(dotPackagesMap['pedantic']!).toFilePath();
-  var lintsLibPath = Uri.parse(dotPackagesMap['lints']!).toFilePath();
-  var pedanticRules =
-      await getRules(join(pedanticLibPath, 'analysis_options.1.11.0.yaml'));
-  await _writeRules('pedantic', pedanticRules);
+  var dartTeamLibPath = join(
+      pathPackageConfigMapGetPackagePath(
+          path, packageConfigMap, 'dart_flutter_team_lints')!,
+      'lib');
+  var lintsDartTeamRecommended =
+      await getRules(join(dartTeamLibPath, 'analysis_options.yaml'));
+  await _writeRules('dart_flutter_team', lintsDartTeamRecommended);
+  var lintsLibPath = join(
+      pathPackageConfigMapGetPackagePath(path, packageConfigMap, 'lints')!,
+      'lib');
+  //var pedanticRules =
+//      await getRules(join(pedanticLibPath, 'analysis_options.1.11.0.yaml'));
+  //await _writeRules('pedantic', pedanticRules);
   var lintsRecommendedRules =
       await getRules(join(lintsLibPath, 'recommended.yaml'));
   await _writeRules('recommended_lints', lintsRecommendedRules);
   var lintsCoreRules = await getRules(join(lintsLibPath, 'core.yaml'));
   await _writeRules('core_lints', lintsCoreRules);
 
+  //var diffRules = List<String>.from(rules);
+  //diffRules.removeWhere((element) => pedanticRules.contains(element));
+  // await _writeRules('pedantic_diff', diffRules);
   var diffRules = List<String>.from(rules);
-  diffRules.removeWhere((element) => pedanticRules.contains(element));
-  await _writeRules('pedantic_diff', diffRules);
-  diffRules = List<String>.from(rules);
   diffRules.removeWhere((element) => lintsRecommendedRules.contains(element));
   await _writeRules('lints_diff', diffRules);
   diffRules = List<String>.from(lintsRecommendedRules);
-  diffRules.removeWhere((element) => pedanticRules.contains(element));
-  await _writeRules('lints_over_pedantic', diffRules);
-  diffRules = List<String>.from(pedanticRules);
-  diffRules.removeWhere((element) => lintsRecommendedRules.contains(element));
-  await _writeRules('pedantic_over_lints', diffRules);
+  //diffRules.removeWhere((element) => pedanticRules.contains(element));
+  //await _writeRules('lints_over_pedantic', diffRules);
+  //diffRules = List<String>.from(pedanticRules);
+  //diffRules.removeWhere((element) => lintsRecommendedRules.contains(element));
+  //await _writeRules('pedantic_over_lints', diffRules);
 
   var all = <String>{}
     ..addAll(rules)
-    ..addAll(pedanticRules)
+    //..addAll(pedanticRules)
     ..addAll(lintsRecommendedRules)
     ..addAll(lintsCoreRules);
   diffRules = List<String>.from(all)..sort();
