4b9cc9e22c975292efa6947ecb2061db56317425
Alexandre Roux <alex@tekartik.com>
2024-09-25 23:37:07 +0200

    feat: list all rules

---
+++ packages/lints/tool/list_all_rules.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:process_run/shell.dart';
+
+Future<void> main() async {
+  var files = await Directory('lib')
+      .list()
+      .where((fse) => fse is File && extension(fse.path) == '.yaml')
+      .toList();
+  for (var file in files) {
+    stdout.writeln('# ${file.path}');
+    var content = (await run('tklint list-rules ${file.path}')).first.outText;
+    await File(join('doc', 'all_rules_${basename(file.path)}'))
+        .writeAsString(content);
+  }
+}
